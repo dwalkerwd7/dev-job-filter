203e3f17db29a951b7b8aa091a909ea5b4dd020e
Derek Walker <d.walkerwd7@gmail.com>
2026-04-06 17:55:51 -0600

    updated pipeline with better logging

--- pipeline/pipeline.js
+++ pipeline/pipeline.js
@@ -12,14 +12,17 @@ async function pipeline() {
 
     if (values.should_scrape) {
         console.log("Scraping jobs...");
-        const scraped_jobs = await scrape(parseInt(values.scrape_limit));
-        console.log(`Scraped ${scraped_jobs.length} unique jobs.`);
+        const raw_jobs = await scrape(parseInt(values.scrape_limit));
+        console.log(`raw ${scraped_jobs.length} unique jobs.`);
+        console.log(`The database is updated with ${raw_jobs.length} raw jobs.`);
     }
 
     console.log("\nFiltering jobs...");
-    const filtered_jobs = await filter(scraped_jobs);
-    console.log(`${filtered_jobs.length} jobs passed the filter.`);
+    const filtered_jobs = await filter(raw_jobs);
+    console.log(`The database is updated with ${filtered_jobs.length} filtered jobs.`);
 }
 
 // run pipeline
-pipeline().catch(error => console.error(`Pipeline Error: ${error}`));
+pipeline()
+    .then(_ => console.log("Pipeline complete! Check the dashboard for the updates."))
+    .catch(error => console.error(`Pipeline Error: ${error}`));
