be1ef2ac57145980874bbe5fe64940798482687a
Derek Walker <d.walkerwd7@gmail.com>
2026-04-10 15:05:32 -0600

    extractedText --> jobDesc

--- dashboard/src/models/Job.ts
+++ dashboard/src/models/Job.ts
@@ -3,7 +3,7 @@ import mongoose, { Schema, Document, Model } from "mongoose";
 export interface IJob extends Document {
   title: string;
   company: string;
-  extractedText: string;
+  jobDesc: string;
   tech_stack: string[];
   employee_count: number;
   embedding: number[];
@@ -15,7 +15,7 @@ export interface IJob extends Document {
 const JobSchema = new Schema<IJob>({
   title: { type: String, required: true },
   company: { type: String, required: true },
-  extractedText: { type: String, default: "" },
+  jobDesc: { type: String, default: "" },
   tech_stack: { type: [String], default: [] },
   employee_count: { type: Number, default: 0 },
   embedding: { type: [Number], default: [] },
