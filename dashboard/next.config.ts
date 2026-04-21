a4f3af9eb360ab37643210426ef6c5514c31f0b1
Derek Walker <d.walkerwd7@gmail.com>
2026-04-21 01:27:12 -0600

    turns out need this

--- dashboard/next.config.ts
+++ dashboard/next.config.ts
@@ -1,5 +1,8 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+    basePath: process.env.BASE_PATH || "",
+    output: "standalone"
+};
 
 export default nextConfig;
