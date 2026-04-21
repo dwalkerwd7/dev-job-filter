ad1ea220763f47735675f5be27bb47e4b2e4adc4
Derek Walker <d.walkerwd7@gmail.com>
2026-04-21 01:17:55 -0600

    .

--- dashboard/next.config.ts
+++ dashboard/next.config.ts
@@ -1,7 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-    basePath: process.env.BASE_PATH || ""
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
