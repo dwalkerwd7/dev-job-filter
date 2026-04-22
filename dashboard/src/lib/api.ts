cbb56cc0d768d90d245181a1417fe193327174f1
Derek Walker <d.walkerwd7@gmail.com>
2026-04-22 17:19:13 -0600

    don't need this exposed to client-side

--- dashboard/src/lib/api.ts
+++ dashboard/src/lib/api.ts
@@ -1,5 +1,5 @@
 export default function API(subroute: string) {
-    const BASE_PATH = process.env.NEXT_PUBLIC_BASE_PATH;
+    const BASE_PATH = process.env.BASE_PATH;
     if (subroute.startsWith('/')) subroute = subroute.slice(1)
     return `${BASE_PATH}/api/${subroute}`
 }
