063b54b40c798328445de986e2ef1b5c8abc8842
Derek Walker <d.walkerwd7@gmail.com>
2026-05-05 09:57:50 -0600

    rebranding

--- dashboard/src/app/layout.tsx
+++ dashboard/src/app/layout.tsx
@@ -4,38 +4,38 @@ import "./globals.css";
 import TabNav from "@/components/TabNav"
 
 const geistMono = Geist_Mono({
-    variable: "--font-geist-mono",
-    subsets: ["latin"],
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-    title: "Dev Job Filter",
-    description: "An open-source tool by DTS for unemployed software developers.",
+  title: "Dev Job Filter",
+  description: "An open-source tool by DTS for unemployed software developers.",
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <html
-            lang="en"
-            className={`${geistMono.variable} antialiased`}
-        >
-            <body className="min-h-screen flex flex-col flex-1">
-                <TabNav />
-                {children}
-                <footer className="bg-white text-center text-sm text-gray-400 m-w-screen border-t border-gray-100">
-                    &copy; 2026 <a
-                        href="https://derekwalker.tech"
-                        target="_blank"
-                        className="underline text-green-500 hover:text-green-700"
-                    >
-                        DTS
-                    </a>. MIT License
-                </footer>
-            </body>
-        </html>
-    );
+  return (
+    <html
+      lang="en"
+      className={`${geistMono.variable} antialiased`}
+    >
+      <body className="min-h-screen flex flex-col flex-1">
+        <TabNav />
+        {children}
+        <footer className="bg-white text-center text-sm text-gray-400 m-w-screen border-t border-gray-100">
+          &copy; 2026 <a
+            href="https://derekwalker.tech"
+            target="_blank"
+            className="underline text-green-500 hover:text-green-700"
+          >
+            DTL
+          </a>. MIT License
+        </footer>
+      </body>
+    </html>
+  );
 }
